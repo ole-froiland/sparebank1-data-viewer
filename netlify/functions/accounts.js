ad6fd97f91b01c90236f2decd057e811caad14fb
ole-froiland <olealexanderfroiland02@gmail.com>
2025-12-20 21:44:33 +0100

    Fix accounts wrapper handler export

--- netlify/functions/accounts.js
+++ netlify/functions/accounts.js
@@ -1,2 +1,14 @@
-const { handler } = require("./accounts_v2.js");
-exports.handler = handler;
+exports.handler = async (event, context) => {
+  const mod = await import("./accounts_v2.js");
+  if (typeof mod.handler !== "function") {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: true,
+        message: "accounts_v2.handler not exported correctly",
+        exportedKeys: Object.keys(mod || {}),
+      }),
+    };
+  }
+  return mod.handler(event, context);
+};
